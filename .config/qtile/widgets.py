2fe6ac5b0c331d700f1eb88a20d830e1883723b3
ajoino <jacob.nilsson@ltu.se>
2022-08-16 22:59:08 +0200

    Started working on popups but inconsistent Python libraries keep me from going further.

--- .config/qtile/widgets.py
+++ .config/qtile/widgets.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+import subprocess
 
 from libqtile import bar
+from libqtile.lazy import lazy
 from qtile_extras import widget
 from qtile_extras.widget.decorations import RectDecoration
+from qtile_extras.popup.toolkit import PopupRelativeLayout, PopupText, _PopupLayout
 
 from preferences import preferences as pref
 
@@ -18,6 +21,62 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 corner_radius = 10
+
+# Monkeypatch _PopupLayout defaults
+_PopupLayout.defaults = [
+        ("width", 200, "Width of tooltip"),
+        ("height", 200, "Height of tooltip"),
+        ("controls", [], "Controls to display"),
+        ("margin", 5, "Margin around edge of tooltip"),
+        ("background", "000000", "Popup background colour"),
+        ("opacity", 1, "Popup window opacity. 'None' inherits bar opacity"),
+        ("close_on_click", True, "Hide the popup when control is clicked"),
+        (
+            "keymap",
+            {
+        #        "left": ["Left", "h"],
+        #        "right": ["Right", "l"],
+        #        "up": ["Up", "j"],
+        #        "down": ["Down", "k"],
+        #        "select": ["Return", "space"],
+        #        "step": ["Tab"],
+        #        "close": ["Escape"],
+            },
+            "Keyboard controls. NB Navigation logic is very rudimentary. The popup will try "
+            "to select the nearest control in the direction pressed but some controls may be "
+            "inaccessible. In that scenario, use the mouse or `Tab` to cycle through controls.",
+        ),
+        ("keyboard_navigation", True, "Whether popup controls can be navigated by keys"),
+        ("initial_focus", 0, "Index of control to be focused at startup."),
+    ]  # type: list[tuple[str, Any, str]]
+
+@lazy.function
+def show_upgradable_packages(qtile):
+    res = subprocess.run("aptitude search '~U' -F".split())
+    #if res.returncode != 0:
+    #    raise RuntimeError("Return code of \"aptitude search '~U'\" is not 0")
+
+    text = res.stdout
+
+    controls = [
+        PopupText(
+            text=text,
+            pos_x=0.0,
+            pos_y=0.0,
+            width=1.0,
+            height=1.0,
+        )
+    ]
+
+    layout = PopupRelativeLayout(
+        qtile,
+        width=200,
+        height=100,
+        controls=controls,
+        initial_focus=None,
+    )
+
+    layout.show()
 
 def separator(width=2):
     return widget.Spacer(
@@ -36,6 +95,8 @@ def widget_fill(radius=corner_radius):
 
 left_box = [corner_radius, 0, 0, corner_radius]
 right_box = [0, corner_radius, corner_radius, 0]
+
+
 
 widgets = [
     separator(4),
@@ -72,8 +133,11 @@ widgets = [
         display_format="↻ {updates}",
         restart_indicator=" (!)",
         update_interval=60,
-        #execute=f"aptitude search '~U'",
         decorations=[widget_fill(left_box)],
+        #execute=show_upgradable_packages,
+        mouse_callbacks={
+            "Button1": show_upgradable_packages,
+        },
     ),
     separator(),
     widget.KeyboardLayout(
