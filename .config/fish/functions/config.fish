d26e4329c5896b600054f32080c80da7348b8fa9
Jacob Nilsson <jacob@flasheye.se>
2022-08-26 23:36:52 +0200

    Make config.fish username agnostic.

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
 function config -wraps=/usr/bin/git
-	/usr/bin/git --git-dir=/home/jacnil/.dotfiles/ --work-tree=/home/jacnil $argv
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
 end
