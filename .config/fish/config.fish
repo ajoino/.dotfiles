a770d54bc224b9b7111e70cd6156f69a91ba4dfd
Jacob Nilsson <jacob@flasheye.se>
2022-08-27 22:10:04 +0200

    Made fish config autosource ROS using bass.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+bass source /opt/ros/noetic/setup.bash
+if test -e ./devel/setup.bash
+  bass source ./devel/setup.bash
+end
